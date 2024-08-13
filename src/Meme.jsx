62c18617abbe279af662dfdfd0f9fdb8e1ca7ec6
DevSankale <sankaledev@gmail.com>
2024-08-13 15:30:13 +0300

    Usestate updated.

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -3,21 +3,28 @@ import memesData from "./memesData";
 
 
 export default function Meme(){
-
-const [memeImage,setMemeImage]=React.useState("")
+const [allMemeImages,setAllMemeImages]=React.useState(memesData);
+const [meme,setMemeImage]=React.useState({
+  topText:"",
+  bottomText:"",
+  randomImage:"https://i.imgflip.com/30b1gx.jpg"
+});
         function getMeme(){
                       
          let url;
                         
-         const memeArray = memesData.data.memes;
+         const memeArray = allMemeImages.data.memes;
          const randomMeme = Math.floor(Math.random()*memeArray.length);
          url = memeArray[randomMeme].url;
                           
-          setMemeImage(url)
-          console.log(url);
-                          
+          setMemeImage((prevMemeImage)=>{
+           return   {
+                ...prevMemeImage,
+                randomImage:url
+              }
+            
+          });
         };
-
 
   return(
     <>
@@ -32,7 +39,7 @@ const [memeImage,setMemeImage]=React.useState("")
       <button className="button" onClick ={getMeme}>Get a new meme image</button>
     </div>
     <div className="memeImage">
-      <img src={memeImage} width="350px" height="auto"/>
+      <img src={meme.randomImage} width="350px" height="auto"/>
     </div>
  
     </>
